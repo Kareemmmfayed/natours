c93b459a0966763d0ff5ed7f74987cc8aa0a8b11
kareem <124769561+Kareemmmfayed@users.noreply.github.com>
2024-07-20 22:47:13 +0300

    Add the AppError class

--- app.js
+++ app.js
@@ -24,4 +24,16 @@ app.all('*', (req, res, next) => {
   next();
 });
 
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+
+  next();
+});
+
 module.exports = app;
