f3f9a83a17036f2c3befae194422c343462e6247
kareem <124769561+Kareemmmfayed@users.noreply.github.com>
2024-07-25 07:37:53 +0300

    Add roles

--- models/userModel.js
+++ models/userModel.js
@@ -15,6 +15,11 @@ const userSchema = new mongoose.Schema({
     validate: [validator.isEmail, 'Please provide a valid E-mail'],
   },
   photo: String,
+  role: {
+    type: String,
+    enum: ['user', 'guide', 'lead-guide', 'admin'],
+    default: 'user',
+  },
   password: {
     type: String,
     required: [true, 'Please provide a password'],
@@ -52,15 +57,16 @@ userSchema.methods.correctPassword = async function (
   return await bcrypt.compare(candidatePassword, userPassword);
 };
 
-userSchema.methods.changedPasswordAfter = async function (JWTTimeStamp) {
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
   if (this.passwordChangedAt) {
-    const changedTimeStamp = parseInt(
+    const changedTimestamp = parseInt(
       this.passwordChangedAt.getTime() / 1000,
       10
     );
 
-    return JWTTimeStamp < changedTimeStamp;
+    return JWTTimestamp < changedTimestamp;
   }
+
   return false;
 };
 
