08b72b92393f06c97aaffbb6fe2d9dc5c9a4dc8b
kareem <124769561+Kareemmmfayed@users.noreply.github.com>
2024-07-26 06:02:45 +0300

    Prevent Parameter Pollution

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const reteLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -32,6 +33,19 @@ app.use(express.json({ limit: '10kb' }));
 app.use(mongoSanitize());
 
 app.use(xss());
+
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
 
 app.use(express.static(`${__dirname}/public`));
 
