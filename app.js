9df79f015fb0391ece3515a1e1eee70cf2fbb7c8
kareem <124769561+Kareemmmfayed@users.noreply.github.com>
2024-07-26 05:46:44 +0300

    Add Data Sanitization

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const reteLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -26,6 +28,10 @@ const limiter = reteLimit({
 app.use('/api', limiter);
 
 app.use(express.json({ limit: '10kb' }));
+
+app.use(mongoSanitize());
+
+app.use(xss());
 
 app.use(express.static(`${__dirname}/public`));
 
