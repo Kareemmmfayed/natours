d545332c53019c614ce29e97a79d89a9b02e9d46
kareem <124769561+Kareemmmfayed@users.noreply.github.com>
2024-07-26 05:35:51 +0300

    Set Security HTTP Headers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const reteLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -9,6 +10,8 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
+
+app.use(helmet());
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -22,7 +25,8 @@ const limiter = reteLimit({
 
 app.use('/api', limiter);
 
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/tours', tourRouter);
